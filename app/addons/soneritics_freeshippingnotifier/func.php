01eb9cf80a9126b34c30b3e50e94e7d9d78222c0
Jordi Jolink <info@soneritics.nl>
2019-07-16 12:32:22 +0200

    Show the correct price

--- app/addons/soneritics_freeshippingnotifier/func.php
+++ app/addons/soneritics_freeshippingnotifier/func.php
@@ -1,2 +1,18 @@
 <?php
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
+
+/**
+ * @param $subtotal
+ * @param int|null $shippingcost
+ * @return float
+ */
+function fn_soneritics_freeshippingnotifier_get_amount($subtotal, $shippingcost = null)
+{
+    // If the shipping cost are known and 0, then you already have free shiping
+    if ($shippingcost == 0) {
+        return 0;
+    }
+
+    $freeshipping = \Tygh\Registry::get('addons.soneritics_freeshippingnotifier.amount');
+    return max(0, $freeshipping - $subtotal);
+}
--- design/themes/[YOUR_THEME]/templates/addons/soneritics_freeshippingnotifier/hooks/notification/product_added.post.tpl
+++ design/themes/[YOUR_THEME]/templates/addons/soneritics_freeshippingnotifier/hooks/notification/product_added.post.tpl
@@ -1,4 +1,4 @@
-{assign var="amount_till_free_shipping" value="15.95"}
+{assign var="amount_till_free_shipping" value=fn_soneritics_freeshippingnotifier_get_amount($smarty.session.cart.subtotal, $smarty.session.cart.shipping_cost)}
 
 {if $amount_till_free_shipping > 0}
     <div class="soneritics_free_shipping_text soneritics_free_shipping_notification">
